79650bef8b76898bf5fea82db5a1eb9be36505ac
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-09-01 11:46:06 -0500

    Functionality 1 and 2

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import praw
+from praw.models import MoreComments
+
 # Instantiate an instance of PRAW's Reddit class
 reddit = praw.Reddit(client_id = 'T6u5hwv_zwez-RrT33NCaA', 
                     client_secret = 'B--LwQpjQDaDCjMmmhkcPIsy61GrMQ', 
@@ -6,14 +8,26 @@ reddit = praw.Reddit(client_id = 'T6u5hwv_zwez-RrT33NCaA',
                     password = 'kOV7Nc5#m',
                     user_agent = 'badger-bot-2022')
 
-# submission = reddit.subreddit("test").submit("Test Submission", url="https://reddit.com")
+# Functionality:
+# 1. If a comment is made to r/CFB with 'Minnesota' in the title, reply to the comment "Paul Bunyan's Axe is ours! On Wisconsin!"
+# 2. If a comment is made to r/CFB with 'Wisconsin' in the title, reply to the comment "Paul Bunyan's Axe is ours! On Wisconsin!"
 
-# submission.reply(body="Test Reply")
+CFB_subreddit = reddit.subreddit('CFB')
 
-# for moderator in reddit.subreddit("uwmadison").moderator():
-#     print(moderator)
+# Iterate through the last 10 hot posts that are not stickied in CFB_subreddit
+for submission in CFB_subreddit.hot(limit=10):
+    # If the submission is stickied, skip it
+    if not submission.stickied:
+        # Iterate through all comments in the submission
+        for comment in submission.comments:
+            # If the comment is a MoreComments object, skip it REF: https://praw.readthedocs.io/en/latest/tutorials/comments.html
+            if isinstance(comment, MoreComments):
+                continue
+            # If the comment contains the word 'Minnesota', reply to the comment
+            if 'Minnesota' in comment.body:
+                comment.reply(body ="Paul Bunyan's Axe is ours! On Wisconsin!")
+            # If the comment contains the word 'Wisconsin', reply to the comment
+            elif 'Wisconsin' in comment.body:
+                comment.reply(body="Paul Bunyan's Axe is ours! On Wisconsin!")
 
-# Repy to the weekly top comment for the subreddit "CFB" with "Go Badgers! On Wisconsin!"
-for comment in reddit.subreddit("CFB").top(time_filter ="week"):
-    comment.reply(body ="Go Badgers! On Wisconsin!")
-    break
+
